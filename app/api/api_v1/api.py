f40c5a3b29e858c59f83249a8e383b808e4be0c2
Adam Lisowski <48516273+adamfoxman@users.noreply.github.com>
2022-02-07 13:03:51 +0100

    deleted Item references

--- app/api/api_v1/api.py
+++ app/api/api_v1/api.py
@@ -1,11 +1,10 @@
 from fastapi import APIRouter
 
-from app.api.api_v1.endpoints import items, login, users, utils, transmitters, countries
+from app.api.api_v1.endpoints import login, users, utils, transmitters, countries
 
 api_router = APIRouter()
 api_router.include_router(login.router, tags=["login"])
 api_router.include_router(users.router, prefix="/users", tags=["users"])
 api_router.include_router(utils.router, prefix="/utils", tags=["utils"])
-# api_router.include_router(items.router, prefix="/items", tags=["items"])
 api_router.include_router(transmitters.router, prefix="/transmitters", tags=["transmitters"])
 api_router.include_router(countries.router, prefix="/countries", tags=["countries"])
